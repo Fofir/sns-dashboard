076250924f4d4654744d6fd81d9c1be807dff099
Fofir <ofir.weinstock@gmail.com>
2017-02-09 16:27:44 +0100

    Fixed create job endpoint to return only the created job

--- server/source/controllers/index.js
+++ server/source/controllers/index.js
@@ -4,53 +4,57 @@ const AGENDA_NAME = require('../constants').AGENDA_NAME;
 const sns = new AWS.SNS();
 
 module.exports = (agenda) => ({
-	// lists all jobs
-	listJobs: (req, res) => {
-  	agenda.jobs({ name: AGENDA_NAME }, function(err, jobs) {
+  // lists all jobs
+  listJobs: (req, res) => {
+    agenda.jobs({ name: AGENDA_NAME }, function(err, jobs) {
       if (err) {
-      	return res.send(err);
+        return res.send(err);
       }
       
       return res.json(jobs);
     });
-	},
+  },
 
-	// Creates a job 
-	createJob: (req, res) => {
-		const time = req.body.time;
-  	const job = agenda.schedule(time, AGENDA_NAME, req.body);
-    return res.json({ message: 'Job created succesfully', job });
-	},
+  // Creates a job 
+  createJob: (req, res) => {
+    const time = req.body.time;
+    const job = agenda.schedule(time, AGENDA_NAME, req.body, (err, job) => {
+      if (err) {
+        return res.send(err);
+      };
+      
+      return res.json(job);
+    });
+  },
 
-	// Deletes a job using Agenda
-	deleteJob: (req, res) => {
+  // Deletes a job using Agenda
+  deleteJob: (req, res) => {
+    const _id = mongodb.ObjectID(req.params.id);
+    agenda.jobs({ _id }, (err, jobs) => {
+      if (!jobs || !jobs.length) {
+        return res.send('Not found');
+      }
 
-		const _id = mongodb.ObjectID(req.params.id);
-		agenda.jobs({ _id }, (err, jobs) => {
-				if (!jobs || !jobs.length) {
-					return res.send('Not found');
-				}
+      const job = jobs[0];
 
-				const job = jobs[0];
+      job.remove(function(err) {
+          if(err) {
+            res.send(err);
+          }
 
-				job.remove(function(err) {
-				    if(err) {
-				    	res.send(err);
-				    }
+          return res.json({ message: 'Job deleted successfully' });
+      })
+    });
+  },
 
-	      		return res.json({ message: 'Job deleted successfully' });
-				})
-		});
-	},
+  // Gets all user topics from AWS
+  getTopics: (req, res) => {
+    sns.listTopics({}, function(err, data) {
+      if (err) {
+        return res.send(err);
+      }
 
-	// Gets all user topics from AWS
-	getTopics: (req, res) => {
-		sns.listTopics({}, function(err, data) {
-  		if (err) {
-  			return res.send(err);
-  		}
-
-  		return res.json(data);
-  	});
-	}
+      return res.json(data);
+    });
+  }
 })

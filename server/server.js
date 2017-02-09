453a566bef945a11f5f72b770e38b669f8810b22
Fofir <ofir.weinstock@gmail.com>
2017-02-09 10:21:18 +0100

    Added REST routes and placeholders for their controllers

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const controllers = require('./source/controllers');
 
 // @TODO: Add error handling for failed connection
 mongoose.connect(process.env.DB_URI);
@@ -11,6 +12,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
+
+const apiRouter = express.Router();
+
+apiRouter.route('/topics')
+  .get(controllers.getTopics);
+
+apiRouter.route('/jobs')
+  .get(controllers.listJobs)
+  .post(controllers.createJob);
+
+apiRouter.route('/jobs/:id')
+  .delete(controllers.deleteJob);
+
+app.use('/api', apiRouter);
 
 app.listen(port);
 console.log('connected on ' + port);
---
+++ server/source/controllers/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	// lists all jobs
+	listJobs: (req, res) => {
+
+	},
+
+	// Creates a job 
+	createJob: (req, res) => {
+
+	},
+
+	// Deletes a job (soft delete)
+	deleteJob: (req, res) => {
+
+	},
+
+	// Gets all user topics from AWS
+	getTopics: (req, res) => {
+	}
+}
